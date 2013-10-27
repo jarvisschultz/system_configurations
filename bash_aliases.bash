c0e7a3b19f3daef580daef16162eece0f702d03b
Jarvis Schultz <jschultz@u.northwestern.edu>
2013-10-27 10:58:14 -0500

    Added a check for args to the rmhist function.

--- bash_aliases.bash
+++ bash_aliases.bash
@@ -1,6 +1,12 @@
 function rmhist() {
     start=$1
     end=$2
+    if [[ -z "$start" || -z "$end" ]];
+    then
+	echo "Bad args!"
+	echo "Usage: rmhist start stop"
+	return
+    fi
     count=$(( end - start ))
     while [ $count -ge 0 ] ; do
         history -d $start

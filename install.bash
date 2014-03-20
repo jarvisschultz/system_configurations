12c47cdce30d653a30b11a16dd04bd87282a5adc
Jarvis Schultz <jschultz@u.northwestern.edu>
2014-03-20 13:08:23 -0500

    Forgot to add the byobu_config dir, and did some re-organization.

--- install.bash
+++ install.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+##################################################
 # bash stuff
 ln -svbi $PWD/bashrc.bash $HOME/.bashrc
 ln -svbi $PWD/bash_aliases.bash $HOME/.bash_aliases
@@ -7,38 +9,46 @@ if [ ! -d $HOME/bin ]
 then
     mkdir $HOME/bin
 fi
-# misc bash scripts
 for f in $PWD/my_bash_scripts/*
 do
     ln -svbi ${f} $HOME/bin/
 done
-# misc display scripts
 for f in $PWD/my_display_scripts/*
 do
     ln -svbi ${f} $HOME/bin/
 done
+##################################################
 
-# conkeror
-ln -svbi $PWD/conkerorrc.conf $HOME/.conkerorrc
 
+##################################################
 # # emacs
-# ln -svbi $PWD/dotemacs.el $HOME/.emacs
+ln -svbi $PWD/dotemacs.el $HOME/.emacs
 if [ ! -d $HOME/.emacs.d/misc-packages ]
 then
     mkdir $HOME/.emacs.d/misc-packages
 fi
-# misc bash scripts
 for f in $PWD/my_emacs_scripts/*
 do
     ln -svbi ${f} $HOME/.emacs.d/misc-packages/
 done
+##################################################
 
+
+##################################################
 # ssh
 ln -svbi $PWD/sshpublickey $HOME/.ssh/id_rsa.pub
-
 # screen and tmux
 ln -svbi $PWD/screenrc.conf $HOME/.screenrc
-ln -svbi $PWD/tmux.conf $HOME/.tmuxrc
-
+ln -svbi $PWD/tmux.conf $HOME/.tmux.conf
 # xbindkeys
 ln -svbi $PWD/xbindkeysrc $HOME/.xbindkeysrc
+# byobu
+if [ ! -d $HOME/.byobu ]
+then
+    mkdir $HOME/.byobu
+fi
+for f in $PWD/byobu-config/*
+do
+    ln -svbi ${f} $HOME/.byobu/
+done
+##################################################

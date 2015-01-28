00b39c9a6dd2f369c83b3213aacf0294c900a77b
Jarvis Schultz <schultzjarvis@gmail.com>
2015-01-28 16:38:12 -0600

    Added groups to the customizable vars

--- my_emacs_scripts/zoom-fonts.el
+++ my_emacs_scripts/zoom-fonts.el
@@ -1,12 +1,21 @@
+(defgroup zoom nil
+  "Customization group for `zoom-fonts' package"
+  :version 0.1
+  :group 'frames
+  )
+
 (defcustom zoom-big-size 140
   "Default face height for the `zoom-on' function."
-  :type 'int)
+  :type 'integer
+  :group 'zoom)
 (defcustom zoom-small-size 100
   "Default face height for the `zoom-off' function."
-  :type 'int)
+  :type 'integer
+  :group 'zoom)
 (defcustom zoom-increment-size 20
   "Default face height change the `zoom-in' and `zoom-out' functions."
-  :type 'int)
+  :type 'integer
+  :group 'zoom)
 
 (defun zoom-on ()
   "Set face height to be `zoom-big-size' for all faces in all frames"

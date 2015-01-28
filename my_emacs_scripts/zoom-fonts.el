b20748b653b987ef81ecec10b51a3cbf73e1bdb4
Jarvis Schultz <schultzjarvis@gmail.com>
2015-01-28 16:27:30 -0600

    Added zoom in/out functions and some custom vars for setting the sizes and increments of these functions.

--- my_emacs_scripts/zoom-fonts.el
+++ my_emacs_scripts/zoom-fonts.el
@@ -1,9 +1,33 @@
+(defcustom zoom-big-size 140
+  "Default face height for the `zoom-on' function."
+  :type 'int)
+(defcustom zoom-small-size 100
+  "Default face height for the `zoom-off' function."
+  :type 'int)
+(defcustom zoom-increment-size 20
+  "Default face height change the `zoom-in' and `zoom-out' functions."
+  :type 'int)
+
 (defun zoom-on ()
+  "Set face height to be `zoom-big-size' for all faces in all frames"
   (interactive)
-  (set-face-attribute 'default nil :height 140))
+  (set-face-attribute 'default nil :height zoom-big-size))
 
 (defun zoom-off ()
+  "Set face height to be `zoom-small-size' for all faces in all frames"
   (interactive)
-  (set-face-attribute 'default nil :height 100))
+  (set-face-attribute 'default nil :height zoom-small-size))
+
+(defun zoom-in ()
+  "Increase face height by `zoom-increment-size' for all faces in all frames"
+  (interactive)
+  (set-face-attribute 'default nil :height
+	(+ zoom-increment-size (face-attribute 'default :height nil 'default))))
+
+(defun zoom-out ()
+  "Decrease face height by `zoom-increment-size' for all faces in all frames"
+  (interactive)
+  (set-face-attribute 'default nil :height
+	(- zoom-increment-size (face-attribute 'default :height nil 'default))))
 
 (provide 'zoom-fonts)

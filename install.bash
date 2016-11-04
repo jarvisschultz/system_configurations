452a1517bb3a6d7b0c2660cdd8c064a0dda0340f
Jarvis Schultz <schultzjarvis@gmail.com>
2016-11-04 07:52:50 -0500

    Added some custom git aliases
    Also I now track my gitconfig (minus the username and pw)
    
    Aliases found here:
    https://hackernoon.com/lesser-known-git-commands-151a1918a60
    Talk about an awesome post.

--- install.bash
+++ install.bash
@@ -51,4 +51,22 @@ do
 done
 # global .gitignore
 ln -svbi $PWD/globalgitignore.conf $HOME/.gitignore
+# global .gitconfig
+if [ -f $HOME/.gitconfig ]
+then
+	echo -n "Do you want to add custom Git commands to your Git config? (y/n)? "
+	old_stty_cfg=$(stty -g)
+	stty raw -echo
+	answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+	stty $old_stty_cfg
+	if echo "$answer" | grep -iq "^y" ;then
+		echo "Answered yes!"
+		echo "" >> $HOME/.gitconfig
+		echo "[include]" >> $HOME/.gitconfig
+		echo "	path = .gitcustomconfig" >> $HOME/.gitconfig
+	else
+		echo "Answered no!"
+	fi
+fi
+ln -svbi $PWD/gitconfig.conf $HOME/.gitcustomconfig
 ##################################################
